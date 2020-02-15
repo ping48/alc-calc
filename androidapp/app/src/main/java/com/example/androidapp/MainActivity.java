d28429bb1e9498aad5940d8b53da34418ee096c5
anyu-yu <56010110+anyu-yu@users.noreply.github.com>
2020-02-15 14:57:46 -0800

    error message programmed

--- androidapp/app/src/main/java/com/example/androidapp/MainActivity.java
+++ androidapp/app/src/main/java/com/example/androidapp/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -28,29 +29,45 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText feet = findViewById(R.id.heightFoot);
                 String feet1 = feet.getText().toString();
-                int feet2 = Integer.parseInt(feet1);
+                int feet2 = 0;
+                if(!(feet1.equals("")))
+                    feet2 = Integer.parseInt(feet1);
                 EditText inch = findViewById(R.id.heightInches);
                 String inch1 = inch.getText().toString();
-                double inch2 = Integer.parseInt(inch1);
+                double inch2 = 0.0;
+                if(!(inch1.equals("")))
+                    inch2 = Integer.parseInt(inch1);
                 double totalInches = (12.0 * (feet2 + 0.0) + inch2);
                 EditText pounds = findViewById(R.id.weight);
                 String pounds1 = pounds.getText().toString();
+                int pounds2 = 0;
+                if(!(pounds1.equals("")))
+                    pounds2 = Integer.parseInt(pounds1);
                 //
                 int radioId = gender.getCheckedRadioButtonId();
-                theButton = findViewById(radioId);
-                double gendVal;
-                if(((String)(theButton.getText())).equals("Male"))
-                    gendVal = 0.73;
-                else
-                    gendVal = 0.66;
+                double gendVal = 0.0;
+                if(radioId != -1) {
+                    theButton = findViewById(radioId);
+                    if ((theButton.getText().toString()).equals("Male"))
+                        gendVal = 0.73;
+                    else
+                        gendVal = 0.66;
+                }
                 //
-                int pounds2 = Integer.parseInt(pounds1);
                 //change this to ButtonPage
                 Intent i = new Intent(MainActivity.this, buttonpage.class);
                 i.putExtra("height", totalInches);
                 i.putExtra("weight", pounds2);
                 i.putExtra("genderValue", gendVal);
-                startActivity(i);
+                TextView warn = findViewById(R.id.warning);
+                if(radioId == -1 || totalInches == 0 || pounds2 == 0){
+                    warn.setText("Please complete all fields");
+                }
+                else{
+                    warn.setText("");
+                    startActivity(i);
+                }
+
             }
         });
     }
