9c41a194e43bc31c93e33a8ffe4958f487ff658d
Yan Qu <yanjqu@gmail.com>
2020-02-16 03:16:07 -0800

    added firebase data extraction

--- androidapp/app/src/main/java/com/example/androidapp/Calculator.java
+++ androidapp/app/src/main/java/com/example/androidapp/Calculator.java
@@ -10,6 +10,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class Calculator extends AppCompatActivity {
+    private static long weights;
+    private static double gends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,8 @@ public class Calculator extends AppCompatActivity {
         configuretheButton();
         configureBackButton();
     }
-    private void configuretheButton(){
+
+    private void configuretheButton() {
         Button calcButton = (Button) findViewById(R.id.calcu);
         calcButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,27 +29,35 @@ public class Calculator extends AppCompatActivity {
                 EditText nums = findViewById(R.id.num);
                 String nums1 = nums.getText().toString();
                 double nums2 = 0.0;
-                if(!(nums1.equals("")))
+                if (!(nums1.equals("")))
                     nums2 = Integer.parseInt(nums1);
                 EditText time = findViewById(R.id.timef);
                 String time1 = time.getText().toString();
                 double time2 = 0.0;
-                if(!(time1.equals("")))
+                if (!(time1.equals("")))
                     time2 = Integer.parseInt(time1);
-                String weight = getIntent().getStringExtra("weight");
-                double weights = Double.parseDouble(weight);
-                String gendVal = getIntent().getStringExtra("genderValue");
-                double gends = Double.parseDouble(gendVal);
-                double theBAC = ((nums2 * 3.75) * 5.14 / (weights * gends)) - (0.015 * time2);
-                TextView bacLevel = findViewById(R.id.baclevel);
-                if(time2 == 0 || nums2 == 0)
-                    bacLevel.setText("Please complete all fields");
+                DocumentReference userRef = fr.collection("users").document(userID);
+                userRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+                        weights = (long) document.get("weight");
+                        gends = (double) document.get("gender");
+                    }
+
+                    double theBAC = ((nums2 * 3.75) * 5.14 / (weights * gends)) - (0.015 * time2);
+                    TextView bacLevel = findViewById(R.id.baclevel);
+                if(time2 ==0||nums2 ==0)
+                            bacLevel.setText("Please complete all fields");
                 else
-                    bacLevel.setText("Your BAC is: " + theBAC);
+                        bacLevel.setText("Your BAC is: "+theBAC);
+
+                });
             }
         });
     }
-    private void configureBackButton(){
+
+    private void configureBackButton() {
         Button backButton = (Button) findViewById(R.id.backButton);
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +65,9 @@ public class Calculator extends AppCompatActivity {
                 Intent i = new Intent(Calculator.this, buttonpage.class);
                 startActivity(i);
             }
+
         });
     }
 }
+
+
